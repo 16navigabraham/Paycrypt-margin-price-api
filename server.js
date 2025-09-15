10327e5036295cb5f6e6b9a6bfe4aaa78bad7cf0
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-09-15 15:54:24 +0100

    added send-token-2

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const CACHE_STALE_DURATION = 2 * 60 * 60 * 1000; // 2 hours (still usable)
 // Default tokens to always fetch
 const DEFAULT_TOKENS = [
   'bitcoin', 'ethereum', 'tether', 'usd-coin', 'binancecoin', 
-  'cardano', 'solana', 'polygon', 'chainlink', 'avalanche-2'
+  'cardano', 'solana', 'polygon', 'chainlink', 'send-token-2'
 ];
 
 // Database helper functions
